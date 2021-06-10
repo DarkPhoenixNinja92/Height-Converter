0b4ddca3109d4f097399b1f51d0388b387463e0d
Shaun Pour <scpour@gmail.com>
2021-06-10 11:50:37 -0400

    2

--- js/Height.js
+++ js/Height.js
@@ -1,6 +1,10 @@
-function convertValue() {
+let convertValue = () => {
     let feetInput = document.getElementById("feet-input").value;
     let inchesInput = document.getElementById("inches-input").value;
     let centimeterOutput = (parseInt((feetInput * 12)) + parseInt(inchesInput)) * 2.54 + " Cm.";
     document.getElementById("output-centimeter").append(centimeterOutput);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("height-submit").addEventListener('click', convertValue);
+})
